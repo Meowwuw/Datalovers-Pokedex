c04914e0dcbb8318cdc9f6bbd96e1e291662ee0a
Magenta Paredes <paredesponcemagenta@gmail.com>
2023-03-28 23:26:02 -0500

    Agregar elemento de tipo, separar los valores en span para  asignarles una tipo de clase por valor

--- src/main.js
+++ src/main.js
@@ -10,6 +10,27 @@ const sortOrderSelect = document.getElementById("sortOrder");
 const pokemonsContainer = document.getElementById("pokemons");
 const pokemonFilter = document.getElementById("pokemon-filter");
 
+const typeClasses = {
+  grass: "grass-type",
+  poison: "poison-type",
+  fire: "fire-type",
+  flying: "flying-type",
+  water: "water-type",
+  bug: "bug-type",
+  normal: "normal-type",
+  electric: "electric-type",
+  ground: "ground-type",
+  fairy: "fairy-type",
+  fighting: "fighting-type",
+  psychic: "psychic-type",
+  rock: "rock-type",
+  steel: "steel-type",
+  ice: "ice-type",
+  ghost: "ghost-type",
+  dragon: "dragon-type",
+  dark: "dark-type"
+};
+
 const sortBySelectCallback = () => {
   const sortedPokemons = sortData(
     memory,
@@ -42,20 +63,39 @@ const injectPokemons = (pokemons) => {
   return pokemons.map((obj) => {
     const pokemonCard = document.createElement("div");
     pokemonCard.classList.add("pokemon-card");
+
     const pokemonData = document.createElement("div");
     pokemonData.classList.add("pokemon-data");
+
     const pokemonImage = document.createElement("img");
     pokemonImage.src = obj.img;
     pokemonImage.classList.add("pokemon-image");
+
     const pokemonNum = document.createElement("div");
-    pokemonNum.innerText = obj.num;
+    pokemonNum.innerText = "N ° " + obj.num;
+    pokemonNum.classList.add("pokemon-num");
+
     const pokemonName = document.createElement("div");
-    pokemonName.innerText = obj.name;
+    pokemonName.innerText = obj.name.charAt(0).toUpperCase() + obj.name.slice(1);
+    pokemonName.classList.add("pokemon-name");
+
+    const pokemonType = document.createElement("div");
+    obj.type.forEach((type) => {
+      const typeSpan = document.createElement("span");
+      typeSpan.innerText = type;
+      typeSpan.classList.add("pokemon-type");
+      if (type in typeClasses) {
+        typeSpan.classList.add(typeClasses[type]);
+      }
+      pokemonType.appendChild(typeSpan);
+    });
+    pokemonType.classList.add("pokemon-types");
     
-    pokemonData.append(pokemonImage, pokemonNum, pokemonName);
+    pokemonData.append(pokemonImage, pokemonNum, pokemonName, pokemonType);
     pokemonCard.appendChild(pokemonData);
     return pokemonCard;
   });
+
 };
 
 pokemonsContainer.replaceChildren(...injectPokemons(pokemon));
