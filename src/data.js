bf13da492c5470b86a264bfed55bde490b08331e
Magenta Paredes <paredesponcemagenta@gmail.com>
2023-03-28 23:16:30 -0500

    Agregadas funciones de ejemplo de filtro y ordenamiento de datos

--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const filterData = (data, condition) => {
+  return data.filter((obj) => condition(obj));
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const sortData = (data, sortBy, sortOrder) => {
+  if (sortOrder !== "dsc" && sortOrder !== "asc") {
+    throw new Error("Invalid sortOrder argument");
+  }
+  return data.sort((a, b) => {
+    return sortOrder === "dsc"
+      ? b[sortBy].localeCompare(a[sortBy])
+      : a[sortBy].localeCompare(b[sortBy]);
+  });
 };
