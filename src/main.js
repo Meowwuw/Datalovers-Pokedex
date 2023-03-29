1ab86d89064ed2d5c71e3b2c758a4ee1794d5957
Magenta Paredes <paredesponcemagenta@gmail.com>
2023-03-28 23:14:23 -0500

    Implementar funcionalidad de filtrado y ordenamiento de pokemons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,58 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { filterData, sortData } from "./data.js";
+import data from "./data/pokemon/pokemon.js";
 
-console.log(example, data);
+let { pokemon } = data;
+
+const sortBySelect = document.getElementById("sortBy");
+const sortOrderSelect = document.getElementById("sortOrder");
+
+const pokemonsContainer = document.getElementById("pokemons");
+const pokemonFilter = document.getElementById("pokemon-filter");
+
+sortBySelect.addEventListener("change", () => {
+  let sortedPokemons = sortData(
+    pokemon,
+    sortBySelect.value,
+    sortOrderSelect.value
+  );
+  pokemon = sortedPokemons;
+  sortedPokemons = filterData(pokemon, (pokemon) =>
+    pokemon.name.startsWith(pokemonFilter.value)
+  );
+  pokemonsContainer.replaceChildren(...injectPokemons(sortedPokemons));
+});
+
+sortOrderSelect.addEventListener("change", () => {
+  let reversedPokemons = pokemon.reverse();
+  pokemon = reversedPokemons;
+  reversedPokemons = filterData(pokemon, (pokemon) =>
+    pokemon.name.startsWith(pokemonFilter.value)
+  );
+  pokemonsContainer.replaceChildren(...injectPokemons(reversedPokemons));
+});
+
+pokemonFilter.addEventListener("input", () => {
+  const filteredPokemons = filterData(pokemon, (pokemon) =>
+    pokemon.name.startsWith(pokemonFilter.value)
+  );
+
+  pokemonsContainer.replaceChildren(...injectPokemons(filteredPokemons));
+});
+
+const injectPokemons = (pokemons) => {
+  return pokemons.map((obj) => {
+    const pokemonCard = document.createElement("div");
+    pokemonCard.classList.add("pokemon-card");
+    const pokemonData = document.createElement("div");
+    pokemonData.classList.add("pokemon-data");
+    const pokemonNum = document.createElement("div");
+    pokemonNum.innerText = obj.num;
+    const pokemonName = document.createElement("div");
+    pokemonName.innerText = obj.name;
+    pokemonData.append(pokemonNum, pokemonName);
+    pokemonCard.appendChild(pokemonData);
+    return pokemonCard;
+  });
+};
+
+pokemonsContainer.replaceChildren(...injectPokemons(pokemon));
